e5f86a4e4cf7f2f509bea80b6e9a1ffaadf56ca6
Florian Ewald <florian.ewald@external.sinnerschrader.com>
2018-12-27 21:44:00 +0100

    added webpack to compile scss & future js files
    - restructured webpack folder dir
    - webpack exoports all assets to ./static folder

---
+++ themes/wwweihnachten/webpack.config.js
@@ -0,0 +1,88 @@
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const AssetsPlugin = require('assets-webpack-plugin');
+const assetsPluginInstance = new AssetsPlugin()
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: ['./src/js/app.js', './src/scss/main.scss'],
+  output: {
+    filename: "app.js",
+    path: path.resolve(__dirname, '../../static/js')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        use: [{
+            loader: "file-loader",
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'static/',
+              useRelativePath: true,
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              optipng: {
+                enabled: true,
+              },
+              pngquant: {
+                quality: '65-90',
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 75
+              }
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "../../static/css/styles.css",
+      chunkFilename: "[id].css",
+    }),
+    assetsPluginInstance
+  ],
+  devServer: {
+    port: 3000,
+    open: true
+  },
+  watch: true,
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: 1000,
+    ignored: /node_modules/
+  }
+};
