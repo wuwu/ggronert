0c42e99888259c6dc69a9b453884946565652775
Florian Ewald <florian.ewald@external.sinnerschrader.com>
2018-12-29 23:35:09 +0100

    update theme

--- content_netlify_cms/_index.md
+++ content_netlify_cms/_index.md
@@ -1,4 +1,8 @@
 ---
-title: alles wird gut
+title: Heilkräuter der Alpen
+description: Die wichtigsten Kräter im Portrait
+featured_image: '/images/annie-spratt-746148-unsplash.jpg'
+
 ---
-##index
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer convallis ac sapien a convallis. Integer non libero consequat ex iaculis tristique a ut ante. Sed neque velit, laoreet at accumsan id, vehicula vitae nibh. Nulla finibus tortor sed velit convallis, in porttitor felis efficitur. Cras quam ligula, euismod vel tempor quis, egestas ac diam. Etiam auctor sem ante, eget eleifend erat condimentum sit amet. Quisque efficitur egestas ligula, ac bibendum augue finibus eu. Ut venenatis dictum placerat. Nulla eget diam sodales, eleifend odio non, fermentum lectus. Pellentesque tincidunt interdum ante, sit amet hendrerit ligula mollis vitae. Quisque blandit eleifend enim non fringilla.
--- themes/wwweihnachten/src/js/app.js
+++ themes/wwweihnachten/src/js/app.js
@@ -11,70 +11,92 @@ console.log('krauteratlas start II');
 const $appId = 'QYX4R4E6V5';
 const $apiKey = '59a2c8a7a9cea196a7a3c677b75cffe1';
 
-const search = instantsearch({
-  indexName: 'krauter_compendium',
-  searchClient: algoliasearch($appId, $apiKey),
-  routing: true
-});
 
-
-
-search.start();
 
 window.onload = () => {
   const $hits = document.getElementById('hits');
   const $searchBox = document.getElementById('search-box');
+  const theToggle = document.getElementById('toggle');
   
-  console.log('start')
+  if ($searchBox) {
+    
+    const search = instantsearch({
+      indexName: 'krauter_compendium',
+      searchClient: algoliasearch($appId, $apiKey),
+      routing: true
+    });
 
-  search.addWidget(
-    searchBox({
-      container: $searchBox,
-      placeholder: 'Search'
-    })
-  );
-  search.addWidget(
-    hits({
-      container: $hits,
-      templates: {
-        empty: 'No results',
-        // https://caniuse.com/#feat=template-literals
-        item: '<div class="my-3"><h3><a href="{{ permalink }}">{{{ _highlightResult.title.value }}}</a></h3><div><span class="text-secondary">{{ lastmod_date }}</span> <span class="text-secondary">∙ {{ tags_text }}</span> {{#_highlightResult.description.value}}∙ {{ _highlightResult.description.value }}{{/_highlightResult.description.value}}</div><small class="text-muted">{{ summary }}</small></div>'
-      },
-      transformData: {
-        item: function (data) {
-          data.lastmod_date = new Date(data.lastmod * 1000).toISOString().slice(0, 10)
-          // https://caniuse.com/#search=MAP
-          const tags = data.tags.map(function (value) {
-            return value.toLowerCase().replace(' ', '-')
-          })
-          data.tags_text = tags.join(', ')
-          return data
-        }
-      }
-    })
-  );
-}
-(function iifeMenu(document, window, undefined) {
-  var menuBtn = document.querySelector('.menu__btn');
-  var menu = document.querySelector('.menu__list');
-
-  function toggleMenu() {
-    menu.classList.toggle('menu__list--active');
-    menu.classList.toggle('menu__list--transition');
-    this.classList.toggle('menu__btn--active');
-    this.setAttribute(
-      'aria-expanded',
-      this.getAttribute('aria-expanded') === 'true' ? 'false' : 'true'
+    search.addWidget(
+      searchBox({
+        container: $searchBox,
+        placeholder: 'Search'
+      })
     );
+    search.addWidget(
+      hits({
+        container: $hits,
+        templates: {
+          empty: 'No results',
+          // https://caniuse.com/#feat=template-literals
+          item: '<div class="my-3"><h3><a href="{{ permalink }}">{{{ _highlightResult.title.value }}}</a></h3><div><span class="text-secondary">{{ lastmod_date }}</span> <span class="text-secondary">∙ {{ tags_text }}</span> {{#_highlightResult.description.value}}∙ {{ _highlightResult.description.value }}{{/_highlightResult.description.value}}</div><small class="text-muted">{{ summary }}</small></div>'
+        },
+        transformData: {
+          item: function (data) {
+            data.lastmod_date = new Date(data.lastmod * 1000).toISOString().slice(0, 10)
+            // https://caniuse.com/#search=MAP
+            const tags = data.tags.map(function (value) {
+              return value.toLowerCase().replace(' ', '-')
+            })
+            data.tags_text = tags.join(', ')
+            return data
+          }
+        }
+      })
+    );
+    search.start();
   }
 
-  function removeMenuTransition() {
-    this.classList.remove('menu__list--transition');
+  theToggle.onclick = function () {
+    toggleClass(this, 'on');
+    return false;
   }
+}
 
-  if (menuBtn && menu) {
-    menuBtn.addEventListener('click', toggleMenu, false);
-    menu.addEventListener('transitionend', removeMenuTransition, false);
+
+
+// based on Todd Motto functions
+// https://toddmotto.com/labs/reusable-js/
+
+// hasClass
+function hasClass(elem, className) {
+  return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+}
+// addClass
+function addClass(elem, className) {
+  if (!hasClass(elem, className)) {
+    elem.className += ' ' + className;
   }
-}(document, window));
+}
+// removeClass
+function removeClass(elem, className) {
+  var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, ' ') + ' ';
+  if (hasClass(elem, className)) {
+    while (newClass.indexOf(' ' + className + ' ') >= 0) {
+      newClass = newClass.replace(' ' + className + ' ', ' ');
+    }
+    elem.className = newClass.replace(/^\s+|\s+$/g, '');
+  }
+}
+// toggleClass
+function toggleClass(elem, className) {
+  var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, " ") + ' ';
+  if (hasClass(elem, className)) {
+    while (newClass.indexOf(" " + className + " ") >= 0) {
+      newClass = newClass.replace(" " + className + " ", " ");
+    }
+    elem.className = newClass.replace(/^\s+|\s+$/g, '');
+  } else {
+    elem.className += ' ' + className;
+  }
+}
+
